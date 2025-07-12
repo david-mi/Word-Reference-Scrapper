3bb10c7d47df337d1e741ea77ceefa8435f1a333
David Michel <david.michel2809@gmail.com>
2025-07-12 15:16:48 +0200

    feat: add CustomError and apply it to fetchHtml utility function

--- src/middlewares/errorsHandler.ts
+++ src/middlewares/errorsHandler.ts
@@ -1,5 +1,14 @@
 import { ErrorRequestHandler } from "express";
 
+export const CustomError = class CustomError extends Error {
+  statusCode: number;
+
+  constructor(message: string, statusCode: number = 500) {
+    super(message);
+    this.statusCode = statusCode;
+  }
+};
+
 function formatError(error: unknown): { message: string, statusCode: number; } {
   return {
     message: (error as { message: string; })?.message || "An error has occured",
--- src/utils/fetchHtml/fetchHtml.ts
+++ src/utils/fetchHtml/fetchHtml.ts
@@ -1,8 +1,13 @@
+import { CustomError } from "../../middlewares/errorsHandler.js";
+
 export async function fetchHtml(url: string): Promise<string> {
   const response = await fetch(url);
 
   if (!response.ok) {
-    throw new Error(`HTTP error! status: ${response.status}`);
+    throw new CustomError(
+      "There is no page corresponding to your request",
+      response.status
+    );
   }
 
   const data = await response.text();
