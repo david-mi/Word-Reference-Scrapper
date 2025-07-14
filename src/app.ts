3ce67d0220f277ec2767b42a7fefcd2f454ff25f
David Michel <david.michel2809@gmail.com>
2025-07-14 16:50:23 +0200

    config: change server hostname

--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,8 @@
 import { server } from "./config/server.js";
+const PORT = process.env.PORT
+  ? parseInt(process.env.PORT, 10)
+  : 3000;
 
-server.listen(process.env.PORT, () => {
-  console.log(`Listening on port ${process.env.PORT} (${new Date().toLocaleTimeString("fr-FR")})`);
+server.listen(PORT, "0.0.0.0", () => {
+  console.log(`Listening on port ${PORT} (${new Date().toLocaleTimeString("fr-FR")})`);
 });
